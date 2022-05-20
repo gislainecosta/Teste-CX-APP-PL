113364703dceb4427c3e303c5936763f15a576ad
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-20 02:46:45 -0300

    Creating the address input function and initial ticket list changes

--- app/assets/src/Core.js
+++ app/assets/src/Core.js
@@ -1,11 +1,45 @@
-const client = window.ZAFClient.init()
+const client = ZAFClient.init();
 
 const searchCEP = () => {
   const cep = document.querySelector("#input-cep").value
-  fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then((resp) => resp.json())
-    .then(function (data) {
-      console.log(data);
+  
+  client.get('ticket')
+    .then((res) => {
+      let ticket_id = res.ticket.id
+
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((resp) => resp.json())
+        .then(function (data) {
+          const address = data
+
+          const body = {
+            ticket: {
+              comment: {
+                html_body: `<p style="color: red; font-weight: 800; color: #04363c; font-size: 1rem; text-align: center;">Endereço</p>
+              <p>${address.logradouro}, ${address.complemento}</p>
+              <p>${address.bairro}, ${address.localidade}/${address.uf}</p>
+              <p>${address.cep}</p>`
+              }
+            }
+          }
+
+          client.request({
+            method: 'PUT',
+            url: `/api/v2/tickets/${ticket_id}`,
+            contentType: 'application/json',
+            data: JSON.stringify(body),
+            secure: true
+          }).then(function (data) {
+            console.log("Ticket update successfully", data)
+          }).catch(function (e) {
+            console.log(`Error request ${e}`);
+          })
+
+        })
+        .catch(function (e) {
+          console.log(`Error request ${e}`);
+        })
+
     })
     .catch(function (e) {
       console.log(`Error request ${e}`);
@@ -17,7 +51,7 @@ const listTickets = () => {
   client.get('ticket')
   .then((res) => {
     const id = res.ticket.requester.id
-    
+
     client.request(`/api/v2/users/${id}/tickets/requested`).then((res) => {
       console.log(res.tickets);
     })
