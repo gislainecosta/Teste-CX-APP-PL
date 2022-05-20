1d8d417c83248bee2ba6a6835676520c1042e9ba
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-19 22:17:51 -0300

    Creating base HTML

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -7,9 +7,21 @@ client.metadata().then((metadata) => {
   settings = metadata.settings;
 });
 
+
 const Main = async () => {
+  const functions = Core
   const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+  let appBody = `
+  <div id="main-content">
+    <p>Insira o CEP</p>
+    
+    <section>
+      <input type="number" id="cep" name="cep" placeholder="CEP" />
+      <button onClick=''>Consultar CEP</button>
+    </section>
+    
+    <button onClick=''>Listar Tickets</button>
+  </div>`;
 
   App.innerHTML = appBody;
 };
